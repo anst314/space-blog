85df6785e3dc6c1c49bcfb00c125b1e9812df6f4
Anthony Stanton <anthonystanton314@gmail.com>
2023-05-03 13:29:20 -0400

    removed favicon

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 function createJWT(user) {
